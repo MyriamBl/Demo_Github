23f9d2b3af54254fd20a8c3c0a431d936e40a888
Myriam <mblondy@adesspaysdebrest.infini.fr>
2015-07-13 14:40:01 +0200

    Ajout d'un fichier carte mentale, présentant OuiShare

---
+++ OuiShare.mm
@@ -0,0 +1,125 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="OuiShare" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1434121971118"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="7"/>
+<node TEXT="Qui ?" POSITION="right" ID="ID_250347104" CREATED="1434121127260" MODIFIED="1434121129154">
+<edge COLOR="#ff00ff"/>
+<node TEXT="Une communaut&#xe9; / Un collectif" ID="ID_450244557" CREATED="1434121052453" MODIFIED="1434124433783">
+<node TEXT="Rassemblant tous type de profils" ID="ID_40463366" CREATED="1434121885325" MODIFIED="1434121909092">
+<node TEXT="Chercheurs" ID="ID_983244063" CREATED="1434122150605" MODIFIED="1434122154827"/>
+<node TEXT="Designers" ID="ID_1818622038" CREATED="1434122155078" MODIFIED="1434122157676"/>
+<node TEXT="&#xe9;tudiants" ID="ID_1584368771" CREATED="1434122157917" MODIFIED="1434122162313"/>
+<node TEXT="journalistes" ID="ID_594384245" CREATED="1434122162557" MODIFIED="1434122166761"/>
+<node TEXT="etc." ID="ID_729747700" CREATED="1434122167013" MODIFIED="1434122169891"/>
+</node>
+<node TEXT="Dans plus de 40 pays" ID="ID_1356716742" CREATED="1434124367703" MODIFIED="1434124375987"/>
+<node TEXT="Qui a 4 ans" ID="ID_439741463" CREATED="1434124387007" MODIFIED="1434124401363"/>
+<node TEXT="A diff&#xe9;rents niveaux d&apos;implication" ID="ID_1064848565" CREATED="1434121910598" MODIFIED="1434121920867"/>
+<node TEXT="Ouverte &#xe0; tous" ID="ID_1459672149" CREATED="1434123373279" MODIFIED="1434123382741"/>
+<node TEXT="Cr&#xe9;atrice d&apos;outils permettant de cr&#xe9;er, de reproduire" ID="ID_880614078" CREATED="1434123383302" MODIFIED="1434123406301"/>
+<node TEXT="Qui va l&#xe0; o&#xf9; les gens sont" ID="ID_1910847786" CREATED="1434123414878" MODIFIED="1434123424971"/>
+</node>
+<node TEXT="Un terreau fertile pour les trajectoires personnelles" ID="ID_1091155087" CREATED="1434123342671" MODIFIED="1434123362653"/>
+</node>
+<node TEXT="Ce qui nous rassemble ?" POSITION="right" ID="ID_940879141" CREATED="1434121165837" MODIFIED="1434124477731">
+<edge COLOR="#ffff00"/>
+<node TEXT="Int&#xe9;r&#xea;t/ passion pour la soci&#xe9;t&#xe9; collaborative" ID="ID_1770718146" CREATED="1434121176852" MODIFIED="1434124477727"/>
+</node>
+<node TEXT="Pourquoi ?" POSITION="right" ID="ID_1896056433" CREATED="1434121129748" MODIFIED="1434124291704">
+<edge COLOR="#00ffff"/>
+<node TEXT="Pour accompagner / encourager le d&#xe9;veloppement d&apos;une soci&#xe9;t&#xe9; collaborative" ID="ID_414485873" CREATED="1434121139453" MODIFIED="1434122121924"/>
+<node TEXT="Pour critiquer le contrat social actuel qui ne fonctionne plus, mais surtout r&#xe9;fl&#xe9;chir &#xe0; ce qu&apos;on construit apr&#xe8;s !" ID="ID_864692745" CREATED="1434124321327" MODIFIED="1434124355893"/>
+<node TEXT="Pour imaginer le futur" ID="ID_762888383" CREATED="1434124098495" MODIFIED="1434124105595"/>
+<node TEXT="Pour discuter !" ID="ID_1946191825" CREATED="1434124106038" MODIFIED="1434124120860"/>
+<node TEXT="Pour proposer des choses" ID="ID_996796776" CREATED="1434124300534" MODIFIED="1434124305075"/>
+<node TEXT="Pour faire !" ID="ID_986066590" CREATED="1434124305406" MODIFIED="1434124307715"/>
+<node TEXT="Pour chercher ensemble une vision de la soci&#xe9;t&#xe9; collaborative qui ne soit pas juste un mod&#xe8;le &#xe9;conomique mais un projet de soci&#xe9;t&#xe9;" ID="ID_1141689245" CREATED="1434124150680" MODIFIED="1434124291698"/>
+</node>
+<node TEXT="Quelles activit&#xe9;s ?" POSITION="right" ID="ID_998578566" CREATED="1434121067357" MODIFIED="1434121671773">
+<edge COLOR="#00ff00"/>
+<node TEXT="Une vie communautaire" ID="ID_283380384" CREATED="1434121674421" MODIFIED="1434121707657">
+<node TEXT="Ev&#xe8;nements" ID="ID_272705282" CREATED="1434121707876" MODIFIED="1434121712801"/>
+<node TEXT="Groupes en ligne" ID="ID_1759397991" CREATED="1434121713116" MODIFIED="1434121717293"/>
+<node TEXT="OuiShare Fest" ID="ID_1376252710" CREATED="1434121717548" MODIFIED="1434121722915"/>
+<node TEXT="..." ID="ID_732190927" CREATED="1434121725389" MODIFIED="1434121726378"/>
+</node>
+<node TEXT="Des productions intellectuelles" ID="ID_182855296" CREATED="1434121730517" MODIFIED="1434121762291">
+<node TEXT="Etudes" ID="ID_451407572" CREATED="1434121742389" MODIFIED="1434121745492"/>
+<node TEXT="Publications" ID="ID_776383863" CREATED="1434121745765" MODIFIED="1434121748378"/>
+<node TEXT="Toolkits" ID="ID_1623521716" CREATED="1434121748796" MODIFIED="1434121757833"/>
+<node TEXT="..." ID="ID_1543359892" CREATED="1434121758164" MODIFIED="1434121759105"/>
+</node>
+<node TEXT="L&apos;accompagnement / Incubation de projets" ID="ID_854359896" CREATED="1434121768238" MODIFIED="1434121782890"/>
+<node TEXT="La formation et p&#xe9;dagogie" ID="ID_1295790910" CREATED="1434121783364" MODIFIED="1434121793075">
+<node TEXT="Pour les &#xe9;tudiants" ID="ID_1648922325" CREATED="1434121795653" MODIFIED="1434121802283"/>
+<node TEXT="Pour les entreprises" ID="ID_1981041962" CREATED="1434121802789" MODIFIED="1434121805705"/>
+<node TEXT="Pour les collectivit&#xe9;s" ID="ID_1904438255" CREATED="1434121805964" MODIFIED="1434121809202"/>
+</node>
+</node>
+<node TEXT="Une soci&#xe9;t&#xe9; collaborative c&apos;est ..." POSITION="right" ID="ID_1792962352" CREATED="1434121866853" MODIFIED="1434121990609">
+<edge COLOR="#7c0000"/>
+<node TEXT="Une soci&#xe9;t&#xe9; bas&#xe9;e sur des principes" ID="ID_6133380" CREATED="1434121991981" MODIFIED="1434123152744">
+<node TEXT="d&apos;ouverture/d&apos;inclusion" ID="ID_1741648672" CREATED="1434121999589" MODIFIED="1434124224030"/>
+<node TEXT="de collaboration" ID="ID_634775375" CREATED="1434122066573" MODIFIED="1434122069444"/>
+<node TEXT="de confiance" ID="ID_248215916" CREATED="1434122044958" MODIFIED="1434122047906"/>
+<node TEXT="de partage de valeur" ID="ID_257537837" CREATED="1434122048390" MODIFIED="1434122051340"/>
+<node TEXT="de construction de lien social" ID="ID_1815866287" CREATED="1434122223549" MODIFIED="1434123128650"/>
+<node TEXT="les &#xe9;changes de pair &#xe0; pair" ID="ID_397200959" CREATED="1434122218053" MODIFIED="1434123152743"/>
+<node TEXT="la production de bien commun" ID="ID_1334236637" CREATED="1434122234061" MODIFIED="1434122239713"/>
+</node>
+<node TEXT="Des mod&#xe8;les &#xe9;conomiques &#xe9;mergents qui reposent sur ..." ID="ID_736696248" CREATED="1434122080622" MODIFIED="1434123199809"/>
+<node TEXT="Une soci&#xe9;t&#xe9; qui impacte tous les c&#xf4;t&#xe9;s de la vie" ID="ID_939263563" CREATED="1434123229190" MODIFIED="1434123274282"/>
+<node TEXT="Des mod&#xe8;les &#xe9;conomiques o&#xf9; la technologie autorise les gens &#xe0; obtenir ce qu&apos;ils souhaitent les uns des autres" ID="ID_1728365770" CREATED="1434123274710" MODIFIED="1434123321923"/>
+<node TEXT="Une soci&#xe9;t&#xe9; qui met la valeur d&apos;usage devant la valeur de propri&#xe9;t&#xe9; d&apos;un bien" ID="ID_249878700" CREATED="1434123451343" MODIFIED="1434123465627"/>
+<node TEXT="Un brouillage de la fronti&#xe8;re entre la production (travail) et la consommation (vie priv&#xe9;e)" ID="ID_349639137" CREATED="1434123466174" MODIFIED="1434123505693"/>
+</node>
+</node>
+</map>
